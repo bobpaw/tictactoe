02b24e31d7739d0584e5e6275fc8507c966c2cf9
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-12-29 19:10:04 -0600

    Added the game view

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 let express = require("express");
 const { join } = require("path");
+const morgan = require("morgan");
 
 const app = express();
 const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.set("views", join(__dirname, "views"));
+
+app.use(morgan("dev"));
+
+app.use("/public", express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
 	res.send("Hello world");
 });
 
-app.use("/static", express.static(join(__dirname, "public")));
+app.get("/game", (req, res) => {
+	res.render("index");
+});
 
 app.use(function (req, res) {
 	res.status(404).send("404 Not found.");
