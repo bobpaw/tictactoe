529790d93280c7e30f87fe3a8d4bcabc2df0c369
Aiden Woodruff <aiden.woodruff@gmail.com>
2022-01-17 11:44:28 -0600

    Added test cases
    Added constructor size test
    Added idsFromLine test

--- test/model.test.ts
+++ test/model.test.ts
@@ -5,22 +5,63 @@ import { Model } from "../src/model";
 import { Square } from "../src/square";
 
 describe("Model", function () {
+	it("should be the right size.", function () {
+		for (let i = 0; i < 10; ++i) {
+			const model = new Model(i, Square.Cross);
+
+			expect(model._data).to.have.lengthOf(i * i);
+		}
+	});
+
+	it("should return the correct ids.", function () {
+		const model = new Model(3, Square.Cross);
+
+		expect(model.idsFromLine({
+			type: "row",
+			index: 0
+		})).to.be.an("array").that.deep.equals([0, 1, 2]);
+		expect(model.idsFromLine({
+			type: "row",
+			index: 1
+		})).to.be.an("array").that.deep.equals([3, 4, 5]);
+		expect(model.idsFromLine({
+			type: "row",
+			index: 2
+		})).to.be.an("array").that.deep.equals([6, 7, 8]);
+		expect(model.idsFromLine({
+			type: "column",
+			index: 0
+		})).to.be.an("array").that.deep.equals([0, 3, 6]);
+		expect(model.idsFromLine({
+			type: "column",
+			index: 1
+		})).to.be.an("array").that.deep.equals([1, 4, 7]);
+		expect(model.idsFromLine({
+			type: "column",
+			index: 2
+		})).to.be.an("array").that.deep.equals([2, 5, 8]);
+		expect(model.idsFromLine({
+			type: "diagonal",
+			index: "\\"
+		})).to.be.an("array").that.deep.equals([0, 4, 8]);
+		expect(model.idsFromLine({
+			type: "diagonal",
+			index: "/"
+		})).to.be.an("array").that.deep.equals([2, 4, 6]);
+	});
+
 	it("should output a line.", function () {
 		const model = new Model(3, Square.Cross);
 
 		model.set(0, Square.Nought);
 		model.set(4, Square.Nought);
 		model.set(8, Square.Nought);
-
-		expect(model.readLine({
-			type: "row",
-			index: 0
-		})).to.have.lengthOf(3);
 		
-		expect(model.readLine({
+		expect(model.getLine({
 			type: "row",
 			index: 0
-		})).to.deep.equal([Square.Nought, Square.Blank, Square.Blank]);
+		})).to.be.an("array").of.length(3).that.
+			deep.equals([Square.Nought, Square.Blank, Square.Blank]);
 	});
 
 	it("shouldn't instantly be a win.", function () {
@@ -43,17 +84,6 @@ describe("Model", function () {
 		beforeEach(function () {
 			model = new Model(3, Square.Cross);
 		});
-
-		it("should recognize a top row.");
-		it("should recognize a middle row.");
-		it("should recognize a low row.");
-
-		it("should recognize a left column.");
-		it("should recognize a middle column.");
-		it("should recognize a right column.");
-
-		it("should recognize a TL to LR diagonal.");
-		it("should recognize a LL to TR diagonal.");
 	});
 	
 	describe("4x4", function () {
