58215cf58d85b254396a152b24b8aaf17a4d1486
Aiden Woodruff <aiden.woodruff@gmail.com>
2022-01-16 23:52:49 -0600

    Separated line logic
    getLine and setLine should be preferred
    idsFromLine should be considered private.
    Use Array.fill instead

--- src/model.ts
+++ src/model.ts
@@ -16,32 +16,47 @@ export class Model {
 
 	constructor(size: number, player: PlayType) {
 		this.size = size;
-		this._data = Array(size * size).map(() => Square.Blank);
+		this._data = new Array(size * size).fill(Square.Blank);
 		this.player = player;
 	}
 
-	readLine (line: Line): Square[] {
+	idsFromLine (line: Line): number[] {
+		const ids = [...this._data.keys()];
+
 		if (line.type === "diagonal") {
-			if (line.index === "/")
-				return this._data.filter((_, i) => i % (this.size + 1) === 0);
-			else if (line.index === "\\")
-				return this._data.filter((_, i) => i % (this.size - 1) === 0).slice(1, -1);
+			if (line.index === "/") {
+				return ids.filter(id => id % (this.size - 1) === 0, this).slice(1, -1);
+			} else if (line.index === "\\") {
+				return ids.filter(id => id % (this.size + 1) === 0, this);
+			}
 		} else {
 			if (line.index < 0 || line.index >= this.size)
 				throw new RangeError(`c must be in the range [0, ${this.size}).`);
-			if (line.type === "row")
-				return this._data.slice(
-					line.index as number * this.size,
-					(line.index as number + 1) * this.size
-				);
-			else if (line.type === "column")
-				return this._data.filter((_, i) => i % this.size === this.size);
+			if (line.type === "row" && typeof line.index === "number") {
+				const start = this.size * line.index;
+				return ids.slice(start, start + this.size);
+			} else if (line.type === "column" && typeof line.index === "number")
+				return ids.filter(id => id % this.size === line.index);
 		}
 	}
 
+	// Guaranteed to return an array of length this.size.
+	getLine (line: Line): Square[] {
+		return this.idsFromLine(line).map(id => this.get(id));
+	}
+
+	setLine (line: Line, values: Square[]): void {
+		if (values.length < this.size)
+			throw new Error(`values must be at least length ${this.size}.`);
+		
+		const ids = this.idsFromLine(line);
+		for (let i = 0; i < this.size; ++i)
+			this.set(ids[i], values[i]);
+	}
+
 	distill(line: Line): Square {
-		return this.readLine(line).reduce((prev, cur) => {
-			if (prev === cur) return cur;
+		return this.getLine(line).reduce((prev, cur) => {
+			if (prev === cur) return prev;
 			else return Square.Different;
 		});
 	}
